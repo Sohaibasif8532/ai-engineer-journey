60c76b5fc7647bf5a2898fefc08333f3782d51ca
Sohaib Asif <sohaib.asif8532@gmail.com>
2026-01-22 19:43:52 +0500

    CSV Summary

--- 01-python-basics/csv-summary/csv-summary.py
+++ 01-python-basics/csv-summary/csv-summary.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import os
 
-
-df= pd.read_csv("Data.csv")
+file_path = os.path.join(os.path.dirname(__file__), "Data.csv")
+df = pd.read_csv(file_path)
+print("Rows:",df.shape[0])
+print("Columns:",df.shape[1])
 print(df.head())
+print("Column Names:", df.columns.tolist())
