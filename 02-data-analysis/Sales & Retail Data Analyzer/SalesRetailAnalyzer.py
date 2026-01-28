0c9565dc099a26b75a97cd94c668406385142941
Sohaib Asif <sohaib.asif8532@gmail.com>
2026-01-28 07:20:37 +0500

    WIP

--- 02-data-analysis/Sales & Retail Data Analyzer/SalesRetailAnalyzer.py
+++ 02-data-analysis/Sales & Retail Data Analyzer/SalesRetailAnalyzer.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as pyplot
 import pandas as pd
 import os
 import sys
+from datetime import datetime
 
 
 filedirectory= os.path.dirname(os.path.abspath(__file__))
@@ -56,10 +57,46 @@ class SalesRetailAnalyzer:
         else:
             print("File not found")
 
+    @staticmethod
+    def FilterbyDate(Userdate):
+        if os.path.exists(datapath):
+            data=pd.read_csv(datapath)
+            data["Product"] = data["Product"].str.lower()
+            data["Category"] = data["Category"].str.lower()
+            data["Date"]=pd.to_datetime(data["Date"])
+            for index,row in data.iterrows():
+                if Userdate in str(row["Date"]): 
+                    print("Entries for Date found",row)
+                    break
+            else:
+                print("Date not found")
+        else:
+            print("File not found")
+    @staticmethod
+    def FilterbyProduct(UserProduct):
+        if os.path.exists(datapath):
+            data=pd.read_csv(datapath)
+            data["Product"] = data["Product"].str.lower()
+            data["Category"] = data["Category"].str.lower()
+            for index,row in data.iterrows():
+                if UserProduct in str(row["Product"]): 
+                    print("Entries for Product found",row)
+                    break
+            else:
+                print("Product not found")
+        else:
+            print("File not found")
+    
+
+                
+                
+
+
 
 SalesRetailAnalyzer.TotalSalesSummaryStats()
 SalesRetailAnalyzer.AverageSummaryStats()
 SalesRetailAnalyzer.MaxMinSummaryStats()
 SalesRetailAnalyzer.LenResultsStats()
-
+SalesRetailAnalyzer.FilterbyProduct("laptop")
+SalesRetailAnalyzer.FilterbyDate("2026-01-01")
 
