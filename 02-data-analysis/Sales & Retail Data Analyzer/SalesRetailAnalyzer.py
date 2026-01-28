6545a9237c0c04556c863474e315f6cbe10ac217
Sohaib Asif <sohaib.asif8532@gmail.com>
2026-01-28 06:56:15 +0500

    WIP

---
+++ 02-data-analysis/Sales & Retail Data Analyzer/SalesRetailAnalyzer.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as pyplot
+import pandas as pd
+import os
+import sys
+
+
+filedirectory= os.path.dirname(os.path.abspath(__file__))
+datapath= os.path.join(filedirectory, "Data.csv")
+ResultsPath= os.path.join(filedirectory, "Results.csv")
+
+class SalesRetailAnalyzer:
+    @staticmethod
+    def TotalSalesSummaryStats():
+        if os.path.exists(datapath):
+            data= pd.read_csv(datapath)
+            data["Product"] = data["Product"].str.lower()
+            data["Category"] = data["Category"].str.lower()
+            data["TotalSales"]= data["Quantity"]*data["UnitPrice"]
+            print(data["TotalSales"].sum())
+            data.to_csv(ResultsPath,index=False)
+        else:
+            print("File not found")
+    @staticmethod
+    def AverageSummaryStats():
+        if os.path.exists(datapath):
+            data=pd.read_csv(datapath)
+            data["Product"] = data["Product"].str.lower()
+            data["Category"] = data["Category"].str.lower()
+            data["TotalSales"]=data["Quantity"]*data["UnitPrice"]
+            data["AverageSales"]=data["TotalSales"].mean()
+            data.to_csv(ResultsPath,index=False)
+        else:
+            print("File not found") 
+    @staticmethod
+    def MaxMinSummaryStats():
+        if os.path.exists(datapath):
+            data=pd.read_csv(datapath)
+            data["Product"] = data["Product"].str.lower()
+            data["Category"] = data["Category"].str.lower()
+            data["TotalSales"]=data["Quantity"]*data["UnitPrice"]
+            data["MaxSales"]=data["TotalSales"].max()
+            data["MinSales"]=data["TotalSales"].min()
+            data.to_csv(ResultsPath,index=False)
+        else:
+            print("File not found")
+
+    @staticmethod
+    def LenResultsStats():
+        if os.path.exists(datapath):
+            data=pd.read_csv(datapath)
+            data["Product"] = data["Product"].str.lower()
+            data["Category"] = data["Category"].str.lower()
+            data["TotalSales"]=data["Quantity"]*data["UnitPrice"]
+            data["LengthSales"]=data["TotalSales"].count()+1
+            data.to_csv(ResultsPath,index=False)
+        else:
+            print("File not found")
+
+
+SalesRetailAnalyzer.TotalSalesSummaryStats()
+SalesRetailAnalyzer.AverageSummaryStats()
+SalesRetailAnalyzer.MaxMinSummaryStats()
+SalesRetailAnalyzer.LenResultsStats()
+
+
