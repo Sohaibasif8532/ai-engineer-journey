fda8941ddc826c938c74d6203c2c67a70450da65
Sohaib Asif <sohaib.asif8532@gmail.com>
2026-02-02 07:14:27 +0500

    WIP

--- 02-data-analysis/Exploratory Data Analysis & Feature Engineering for Retail Dataset/EDA_pipeline.py
+++ 02-data-analysis/Exploratory Data Analysis & Feature Engineering for Retail Dataset/EDA_pipeline.py
@@ -1,5 +1,5 @@
 import numpy as np
-import datetime 
+import datetime as dt
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,14 +7,22 @@ import pandas as pd
 filedir=os.path.dirname(os.path.abspath(__file__))
 projectroot=filedir
 inputData=os.path.join(projectroot,"data","input","data.csv")
-outputData=os.path.join(projectroot,"data","output","EDA_Featured_output.csv")
+outputDataGeneral=os.path.join(projectroot,"data","output","EDA_general_stats","EDA_Featured_output.csv")
+output_data_aggregated=os.path.join(projectroot,"data","output","EDA_aggrevated_results","EDA_Featured_output.csv")
+output_data_payment=os.path.join(projectroot,"data","output","EDA_Revenue_output","EDA_Revenue_output.csv")
+output_data_top_customers=os.path.join(projectroot,"data","output","EDA_top_customers","EDA_top_customers.csv")
+output_data_monthly_revenue=os.path.join(projectroot,"data","output","EDA_monthly_revenue","EDA_monthly_revenue.csv")
 logs=os.path.join(projectroot,"logs","EDA_logs.log")
 
 
 class EDA_Pipeline:
-    def __init__(self,inputData,outputData,logs):
+    def __init__(self,inputData,outputDataGeneral,output_data_aggregated,output_data_payment,output_data_top_customers,output_data_monthly_revenue,logs):
         self.inputData=inputData
-        self.outputData=outputData
+        self.outputDataGeneral=outputDataGeneral
+        self.output_data_aggregated=output_data_aggregated
+        self.output_data_payment=output_data_payment
+        self.output_data_top_customers=output_data_top_customers
+        self.output_data_monthly_revenue=output_data_monthly_revenue
         self.logs=logs
 
     def loadData(self):
@@ -39,12 +47,42 @@ class EDA_Pipeline:
             "max":"Maximum"
         }
         Stats=Stats.rename(columns=Friendly_Stats)
-        Stats.to_csv(self.outputData,index=False)
+        TotalRevenue=df.groupby("Category").agg(
+            total_revenue=("Total Spent","sum"),
+            average_order_value=("Total Spent","mean"),
+            revenue_per_category=("Total Spent","count"),
+
+        ).reset_index()
+
+
+        RevenuePerPaymentMethod=df.groupby("Payment Method").agg(
+            Total_Revenue=("Total Spent","sum")
+        ).reset_index()
+
+
+        Top_N_customers_by_spend=df.groupby("Customer ID").agg(
+            TotalSpent=("Total Spent","sum")
+            ).sort_values(by="TotalSpent",ascending=False).reset_index()
+        print(TotalRevenue)
+
+
+        df["Month"] = pd.to_datetime(df["Transaction Date"]).dt.month
+        Monthly_Revenue=df.groupby(df["Month"]).agg(
+            MonthlyRevenue=("Total Spent","sum")
+        ).reset_index()
+        
+        Stats.to_csv(self.outputDataGeneral,index=False)
+        TotalRevenue.to_csv(self.output_data_aggregated,index=False,mode="a")
+        RevenuePerPaymentMethod.to_csv(self.output_data_payment,index=False,mode="a")
+        Top_N_customers_by_spend.to_csv(self.output_data_top_customers,index=False,mode="a")
+        Monthly_Revenue.to_csv(self.output_data_monthly_revenue,index=False,mode="a")
         with open(self.logs, "a") as f:
             f.write("*"*226)
             f.write("\n")
             f.write(Stats.to_string())
             f.write("\n")
+            f.write(TotalRevenue.to_string())
+            f.write("\n")
             f.write("*"*226)
             f.write("\n")
 
@@ -52,6 +90,6 @@ class EDA_Pipeline:
 
 
 
-app=EDA_Pipeline(inputData,outputData,logs)
+app=EDA_Pipeline(inputData,outputDataGeneral,output_data_aggregated,output_data_payment,output_data_top_customers,output_data_monthly_revenue,logs)
 app.loadData()
 app.descriptive_stats()
